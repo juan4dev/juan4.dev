093051579dbf3d39a3ffed925de3aeb6e86f10e3
Juan <72524993+juan4dev@users.noreply.github.com>
2024-10-13 00:48:10 +0200

    Add new components to index.ts and update page imports in index.astro (#27)

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as AboutMe } from './AboutMe.astro';
+export { default as Experience } from './Experience.astro';
+export { default as Hero } from './Hero.astro';
+export { default as TitleSection } from './TitleSection.astro';
+export { default as SectionContainer } from './SectionContainer.astro';
+export { default as Footer } from './Footer.astro';
+export { default as Header } from './Header.astro';
